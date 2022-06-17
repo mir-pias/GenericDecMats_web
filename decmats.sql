2e75c7bc8c1db65da1d29171b07dc6b7eb2831ba
mir-pias <mir.pias01@gmail.com>
2022-06-17 14:17:07 +0200

    json import code added

--- decmats.sql
+++ decmats.sql
@@ -10,3 +10,11 @@ hc_series varchar,
 ordinary varchar,
 origin varchar,
 recipe varchar);
+
+/* import json to db */
+CREATE TABLE tmp (c text);
+
+DecMats=# \copy tmp from 'data.json'
+
+INSERT INTO decmats 
+SELECT q.* FROM tmp, json_populate_record(null::decmats, c::json) AS q;
