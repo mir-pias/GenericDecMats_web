358f0979e52da578e322e07306ca43cceecf421a
mir-pias <mir.pias01@gmail.com>
2022-06-24 15:16:13 +0200

    setup db

--- DecMats_web/DecMats_web/settings.py
+++ DecMats_web/DecMats_web/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'DecMats.apps.DecmatsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -114,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'CET'
 
 USE_I18N = True
 
