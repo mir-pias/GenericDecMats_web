35c4d95a3ada49767803879cf2a2848bb7603bdd
mir-pias <mir.pias01@gmail.com>
2023-03-30 16:53:14 +0200

    urls + gitignore

--- DecMats_web/DecMats_web/urls.py
+++ DecMats_web/DecMats_web/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('DecMats/', include('DecMats.urls')),
+    path('', include('DecMats.urls')),
     path('admin/', admin.site.urls),
 ]
